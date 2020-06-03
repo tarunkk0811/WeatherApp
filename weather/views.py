67ea85c462825eec9f518847d87a4901b546dd83
tarunkk0811 <tarun.never.quit>
2020-06-03 21:21:49 +0530

    Added  API key of openweather

--- weather/views.py
+++ weather/views.py
@@ -3,5 +3,7 @@ from django.http import request
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+    url='https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=4419e6fa99d3919e36dfeeda25467809'
+    city='vijayawada'
     return render(request,'weather/weather.html')
     #return HttpResponse("Hello")
