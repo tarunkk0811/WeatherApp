6d600ba6a6c03491ea2a148f54fcb6bd01d3f68d
tarunkk0811 <tarun.never.quit>
2020-06-04 16:44:56 +0530

    Fetches result using api open weather  and added search results into database

--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import request
-from django.http import HttpResponse
+from . models import City
+import requests
+from. forms import CityForm
+from .  models import City
+from django.shortcuts import render
 # Create your views here.
 def index(request):
     url='https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=4419e6fa99d3919e36dfeeda25467809'
-    city='vijayawada'
-    return render(request,'weather/weather.html')
-    #return HttpResponse("Hello")
+
+    if request.method=='POST':
+        form=CityForm(request.POST)
+        form.save()
+
+    form=CityForm
+
+    weather_data=[]
+    cities=City.objects.all()
+    for city in cities:
+        r=requests.get(url.format(city)).json()
+        city_weather={
+        'city' : city.name,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    context={'city_weather':weather_data,'form':form}
+
+    return render(request,'weather/weather.html',context)
